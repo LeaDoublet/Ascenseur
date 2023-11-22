d40e5f782bc8e3480730aa935a2749e971835a5c
Léa Doublet <leadbtcode@gmail.com>
2023-11-22 09:05:19 +0100

    update ascenseur + personne

--- src/main/java/org/example/Ascenseur.java
+++ src/main/java/org/example/Ascenseur.java
@@ -1,4 +1,46 @@
 package org.example;
 
 public class Ascenseur {
+    private final int capacite = 1;
+    private int positionNumEtage;
+    private int destinationEtage;
+    private boolean porteEstOuverte;
+
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public int getPositionNumEtage() {
+        return positionNumEtage;
+    }
+
+    public void setPositionNumEtage(int positionNumEtage) {
+        this.positionNumEtage = positionNumEtage;
+    }
+
+    public boolean isPorteEstOuverte() {
+        return porteEstOuverte;
+    }
+
+    public void setPorteEstOuverte(boolean porteEstOuverte) {
+        this.porteEstOuverte = porteEstOuverte;
+    }
+
+    public void estAppele(Personne personne){
+        //TODO
+    }
+
+    public void entre(Personne personne){
+        //TODO
+    }
+    public void arriveAEtage(){
+        //TODO
+    }
+    public void buttonDestinationPresse(int etageDestination) {
+        this.destinationEtage = etageDestination;
+    }
+    public void personneSortAscenceur(Personne personne){
+        //TODO
+    }
 }
--- src/main/java/org/example/Personne.java
+++ src/main/java/org/example/Personne.java
@@ -1,4 +1,29 @@
 package org.example;
 
 public class Personne {
+    private int positionActuelleEtage;
+    private int positionSouhaiteeEtage;
+
+    public int getPositionActuelleEtage() {
+        return positionActuelleEtage;
+    }
+
+    public void setPositionActuelleEtage(int positionActuelleEtage) {
+        this.positionActuelleEtage = positionActuelleEtage;
+    }
+
+    public int getPositionSouhaiteeEtage() {
+        return positionSouhaiteeEtage;
+    }
+
+    public void setPositionSouhaiteeEtage(int positionSouhaiteeEtage) {
+        this.positionSouhaiteeEtage = positionSouhaiteeEtage;
+    }
+
+    public Personne(int positionActuelleEtage, int positionSouhaiteeEtage) {
+        this.positionActuelleEtage = positionActuelleEtage;
+        this.positionSouhaiteeEtage = positionSouhaiteeEtage;
+    }
+
+    
 }
